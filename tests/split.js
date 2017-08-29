7c1f681a26c4043b9f180f502c0554fea752452c
John Huang <little78926@gmail.com>
2017-08-29 16:04:56 -0700

    update with split function

---
+++ tests/split.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const HummusRecipe = require('../lib');
+
+describe('Split', () => {
+    it('Split PDF', (done) => {
+        const src = path.join(__dirname, 'materials/compressed.tracemonkey-pldi-09.pdf')
+        const outputDir = path.join(__dirname, 'output');
+        const recipe = new HummusRecipe(src);
+        return recipe
+            .split(outputDir)
+            .endPDF(done);
+    });
+});
