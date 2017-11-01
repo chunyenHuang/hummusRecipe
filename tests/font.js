b5fa36122e9730286643d824fb4c5d9b30e765a2
John Huang <little78926@gmail.com>
2017-11-01 11:41:12 -0700

    #11 Enable to load different font types

--- tests/font.js
+++ tests/font.js
@@ -1,9 +1,13 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const HummusRecipe = require('../lib');
 const htmlCodes = fs.readFileSync(path.join(__dirname, './materials/text.html'), 'utf8');
 
 describe('Font', () => {
+    before(()=>{
+        fs.ensureDirSync(path.join(__dirname, 'materials/fonts'));
+
+    });
     it('Add text with custom fonts', (done) => {
         const output = path.join(__dirname, `output/Add text with custom fonts.pdf`);
         const recipe = new HummusRecipe('new', output, {
@@ -11,17 +15,16 @@ describe('Font', () => {
         });
         recipe
             .createPage('letter-size')
-            .text('Hello World', 'center', 80, {
-                font: 'chinese1'                
+            // http://www.fontpalace.com/font-details/PMingLiU/
+            .text('世界你好 繁體字', 'center', 80, {
+                font: 'PMingLiU'
             })
-            .text('Hello World', 'center', 100, {
-                font: 'handwritten1'                
+            .text('世界你好 简体字', 'center', 100, {
+                font: 'PMingLiU'
             })
-            .text('世界你好', 'center', 150, {
-                font: 'chinese1'
-            })
-            .text('asdfghjkl;', 'center', 200, {
-                font: 'chinese1'
+            // http://fonts.gstatic.com/ea/cwtexkai/v3/download.zip
+            .text('世界你好楷體', 'center', 225, {
+                font: 'cwTeXKai-zhonly'
             })
             .endPage()
             .endPDF(done);
