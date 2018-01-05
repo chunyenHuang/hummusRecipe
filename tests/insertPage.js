468e3fa74d0a735e49cb63bdcc6741654756d2ba
John Huang <little78926@gmail.com>
2018-01-04 17:41:29 -0800

    #19 Fix insert bugs for first page and missing pages

--- tests/insertPage.js
+++ tests/insertPage.js
@@ -2,16 +2,19 @@ const path = require('path');
 const HummusRecipe = require('../lib');
 
 describe('Insert Pages', () => {
-    const taskIPFOP = 'Insert page from other pdf';
-    it(taskIPFOP, (done) => {
+    // https://github.com/galkahana/HummusJS/blob/d4aec0ea9200f702ccea9fcada5a3e955feef65e/src/PDFWriterDriver.cpp#L861
+    // https://github.com/galkahana/HummusJS/blob/5d6537c8ab5f1e607efccba49016e56a15e9399b/src/deps/PDFWriter/PDFDocumentHandler.cpp#L664
+    it('Insert page from other pdf', (done) => {
         const src = path.join(__dirname, 'materials/test.pdf');
         const longPDF = path.join(__dirname, 'materials/compressed.tracemonkey-pldi-09.pdf');
-        const output = path.join(__dirname, `output/${taskIPFOP}.pdf`);
+        const output = path.join(__dirname, `output/Insert page from other pdf.pdf`);
         const recipe = new HummusRecipe(src, output);
         recipe
+            .insertPage(0, longPDF, 3)
             .insertPage(2, longPDF, 3)
             .insertPage(1, longPDF, 10)
             .insertPage(1, longPDF, 4)
+            .insertPage(1, longPDF, 2)
             .insertPage(1, longPDF, 2)
             .endPDF(done);
     });
@@ -24,9 +27,8 @@ describe('Insert Pages', () => {
         for (let page = 1; page <= 14; page++) {
             recipe.insertPage(page, insertSrc, 2)
         }
-        recipe.insertPage(14, insertSrc, 1);        
+        recipe.insertPage(14, insertSrc, 1);
         recipe.insertPage(2, insertSrc, 2);
-        
         recipe.endPDF(done);
     });
 });
