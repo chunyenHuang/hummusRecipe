028a15bf178e9e5c1a90e12aae4ff27510c80cb0
John Huang <little78926@gmail.com>
2018-03-23 17:01:10 -0700

    Fix annotation position bugs for rotated pages.

---
+++ tests/annotation-coordinate.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HummusRecipe = require('../lib');
+
+describe('Annotation: Coordinate', () => {
+    const rotations = [0, 90, 180, 270];
+
+    rotations.forEach(rotation => {
+        it(`${rotation} degree rotation.`, (done) => {
+            const src = path.join(__dirname, `materials/test-L-${rotation}.pdf`)
+            const output = path.join(__dirname, `output/annotation-rotation-${rotation}.pdf`);
+            const recipe = new HummusRecipe(src, output);
+            recipe
+                .editPage(1)
+                .text('Should be printed horizontally', 'center', 'center', {
+                    size: 30,
+                    color: '#ff0000'
+                })
+                .annot('center', 'center', 'Square', {
+                    text: 'Should be printed horizontally on the top of the text',
+                    width: 600,
+                    height: 100,
+                    border: 10,
+                    color: [255, 128, 128],
+                    followOriginalPageRotation: false // if set to true, the annotation will follow the original rotation
+                })
+                .endPage()
+                .endPDF(done);
+        });
+    });
+});
--- tests/modify.js
+++ tests/modify.js
@@ -26,4 +26,29 @@ describe('Modify', () => {
             .endPage()
             .endPDF(done);
     });
+
+    it('Add something to an existing pdf', (done) => {
+        const src = path.join(__dirname, 'materials/test2.pdf')
+        const output = path.join(__dirname, `output/Add something to an existing (with annots).pdf`);
+        const recipe = new HummusRecipe(src, output);
+        recipe
+            .info({
+                author: 'wahaha'
+            })
+            .editPage(1)
+            .text('Add some texts to an existing pdf file', 150, 300)
+            .circle('center', 100, 60, { stroke: '#3b7721', fill: '#0e0e0e', opacity: 0.4 })
+            .circle('center', 100, 30, { stroke: '#0032FF' })
+            .rectangle('center', 'center', 450, 450, {
+                color: [255, 0, 255],
+                opacity: 0.2
+            })
+            .comment('Add 1st comment annotaion', 200, 300)
+            .endPage()
+        recipe
+            .editPage(2)
+            .comment('Add 2nd comment annotaion', 200, 100)
+            .endPage()
+            .endPDF(done);
+    });
 });
