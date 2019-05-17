39032655b8a027953e4356c8dc40b7a4a1b84048
shaehn <hsdaddio@gmail.com>
2019-05-17 11:59:59 -0400

    Recipe can now open password protected files #109
    Previously, Recipe.encrypt would be able to add passwords to generated files, but once they were protected, they could no longer be accessed by subsequent Recipe actions. Theses changes fix that problem.

--- tests/encryption.js
+++ tests/encryption.js
@@ -28,4 +28,69 @@ describe('Encryption', () => {
             .endPDF(done);
     });
 
+    const taskAPP = 'Add permission password';
+    it(taskAPP, (done) => {
+        const src = path.join(__dirname, 'materials/test2.pdf');
+        // const overlayPDF = path.join(__dirname, 'materials/test3.pdf');
+        const output = path.join(__dirname, `output/${taskAPP}.pdf`);
+
+        const recipe = new HummusRecipe(src, output);
+        recipe
+            .encrypt({
+                password: '123',
+            })
+            .endPDF(done);
+    });
+
+    const taskCPF = 'New file with view password';
+    it(taskCPF, (done) => {
+        const output = path.join(__dirname, `output/${taskCPF}.pdf`);
+        const recipe = new HummusRecipe('new', output, {userPassword: '123'});
+        recipe
+            .createPage('letter-size')
+            .text('When creating file, the viewing password (userPassword)', 150, 300)
+            .text('is required for file encryption to occur.', 150, 350)
+            .endPage()
+            .endPDF(done);
+    });
+
+    const taskCPP = 'New file with permission password';
+    it(taskCPP, (done) => {
+        const output = path.join(__dirname, `output/${taskCPP}.pdf`);
+        const recipe = new HummusRecipe('new', output, {password: '123'});
+        recipe
+            .createPage('letter-size')
+            .text('When creating file, an empty viewing password (userPassword)', 150, 300)
+            .text('is required for file encryption to occur.', 150, 350)
+            .endPage()
+            .endPDF(done);
+    });
+
+    const taskCPE = 'New file with edit password';
+    it(taskCPE, (done) => {
+        const output = path.join(__dirname, `output/${taskCPE}.pdf`);
+        const a=3900
+        console.log(`perm: ${a.toString(2)}`)
+        const recipe = new HummusRecipe('new', output, {ownerPassword: '123', userProtectionFlag:3900});
+        recipe
+            .createPage('letter-size')
+            .text('When creating file, an empty viewing password (userPassword)', 150, 300)
+            .text('is required for file encryption to occur.', 150, 350)
+            .endPage()
+            .endPDF(done);
+    });
+
+    const taskMPF = 'Modify file with view password';
+    it(taskMPF, (done) => {
+        const input  = path.join(__dirname, `output/${taskCPF}.pdf`);
+        const output = path.join(__dirname, `output/${taskMPF}.pdf`);
+        const recipe = new HummusRecipe(input, output, {userPassword: '123'});
+
+        recipe
+            .editPage(1)
+            .text('The userPassword is also required to modify the file.', 150, 400)
+            .endPage()
+            .endPDF(done);
+    });
+
 });
