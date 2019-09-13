787f99efb8ef963974266f0e4f5f69d664c509d7
shaehn <hsdaddio@gmail.com>
2019-09-13 16:12:49 -0400

    Introducing expanded color system #134
    This introduces the new color space called 'separation' which will allow users to specify PANTONE colors in their PDF documents.
    The other addition is that color values can now be associated with names.
    
    New color tests have been created and old ones updated.

--- tests/font.js
+++ tests/font.js
@@ -13,17 +13,21 @@ describe('Font', () => {
         });
         recipe
             .createPage('letter-size')
-            // http://www.fontpalace.com/font-details/PMingLiU/
+            .text('http://www.fontpalace.com/font-details/PMingLiU/', 20, 60)
             .text('世界你好 繁體字', 'center', 80, {
-                font: 'PMingLiU'
+                font: 'PMingLiU',
+                textBox: {style: {fill: '%0,28,100,6'}}
             })
             .text('世界你好 简体字', 'center', 100, {
-                font: 'PMingLiU'
+                font: 'PMingLiU',
+                textBox: {style: {fill: '%0,28,100,0'}}
             })
-            // http://fonts.gstatic.com/ea/cwtexkai/v3/download.zip
-            .text('世界你好楷體', 'center', 225, {
-                font: 'cwTeXKai-zhonly'
+            .text('http://fonts.gstatic.com/ea/cwtexkai/v3/download.zip', 20, 200)
+            .text('世界你好楷體', 'center', 220, {
+                font: 'cwTeXKai-zhonly',
+                textBox: {style: {fill: '%0,40,100,0'}}
             })
+            .text('Gold boxes will be empty when fonts unavailable.', 50, 350, {color: 'red'})
             .endPage()
             .endPDF(done);
     });
