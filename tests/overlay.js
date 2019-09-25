87db7846d618a5ba319b999cb6f388049319a3e5
John Huang <little78926@gmail.com>
2019-09-24 22:01:43 -0700

    eslint fix

--- tests/overlay.js
+++ tests/overlay.js
@@ -98,116 +98,102 @@ describe('Modify', () => {
     });
 
 
+    it('Add Overlay from other PDF w/ page_index', (done) => {
+        const src = path.join(__dirname, 'materials/test2.pdf');
+        const overlayPDF = path.join(__dirname, 'materials/test3.pdf');
+        const output = path.join(__dirname, 'output/Add overlay with page.pdf');
 
+        const recipe = new HummusRecipe(src, output);
+        recipe
+            .editPage(1)
+            .overlay(overlayPDF, { page: 1 })
+            .endPage()
+            .endPDF(done);
+    });
 
+    it('Add Overlay from other PDF - position w/ page_index', (done) => {
+        const src = path.join(__dirname, 'materials/issue-28-input.pdf');
+        const overlayPDF = path.join(__dirname, 'materials/test2.pdf');
+        const output = path.join(__dirname, 'output/Add overlay (#28) with page - position.pdf');
 
+        const recipe = new HummusRecipe(src, output);
+        const options = { page: 1 };
+        recipe
+            .editPage(1)
+            .overlay(overlayPDF, 500, 300, options)
+            .endPage()
+            .endPDF(done);
+    });
 
+    it('Add Overlay from other PDF - scale w/ page_index', (done) => {
+        const src = path.join(__dirname, 'materials/issue-28-input.pdf');
+        const overlayPDF = path.join(__dirname, 'materials/test2.pdf');
+        const output = path.join(__dirname, 'output/Add overlay (#28) with page - scale.pdf');
 
+        const recipe = new HummusRecipe(src, output);
+        const options = {
+            keepAspectRatio: true,
+            scale: 3,
+            page: 2
+        };
+        recipe
+            .editPage(1)
+            .overlay(overlayPDF, options)
+            .endPage()
+            .endPDF(done);
+    });
 
-        it('Add Overlay from other PDF w/ page_index', (done) => {
-            const src = path.join(__dirname, 'materials/test2.pdf');
-            const overlayPDF = path.join(__dirname, 'materials/test3.pdf');
-            const output = path.join(__dirname, 'output/Add overlay with page.pdf');
+    it('Add Overlay from other PDF - fitWidth w/ page_index', (done) => {
+        const src = path.join(__dirname, 'materials/issue-28-input.pdf');
+        const overlayPDF = path.join(__dirname, 'materials/test2.pdf');
+        const output = path.join(__dirname, 'output/Add overlay (#28) with page - fitWidth.pdf');
 
-            const recipe = new HummusRecipe(src, output);
-            recipe
-                .editPage(1)
-                .overlay(overlayPDF,{page:1})
-                .endPage()
-                .endPDF(done);
-        });
+        const recipe = new HummusRecipe(src, output);
+        const options = {
+            keepAspectRatio: true,
+            fitWidth: true,
+            page: 2
+        };
+        recipe
+            .editPage(1)
+            .overlay(overlayPDF, options)
+            .endPage()
+            .endPDF(done);
+    });
 
-        it('Add Overlay from other PDF - position w/ page_index', (done) => {
-            const src = path.join(__dirname, 'materials/issue-28-input.pdf');
-            const overlayPDF = path.join(__dirname, 'materials/test2.pdf');
-            const output = path.join(__dirname, 'output/Add overlay (#28) with page - position.pdf');
+    it('Add Overlay from other PDF - fitHeight w/ page_index', (done) => {
+        const src = path.join(__dirname, 'materials/issue-28-input.pdf');
+        const overlayPDF = path.join(__dirname, 'materials/test2.pdf');
+        const output = path.join(__dirname, 'output/Add overlay (#28) with page - fitHeight.pdf');
 
-            const recipe = new HummusRecipe(src, output);
-            const options = {page:1};
-            recipe
-                .editPage(1)
-                .overlay(overlayPDF, 500, 300, options)
-                .endPage()
-                .endPDF(done);
-        });
+        const recipe = new HummusRecipe(src, output);
+        const options = {
+            keepAspectRatio: true,
+            fitHeight: true,
+            page: 1
+        };
+        recipe
+            .editPage(1)
+            .overlay(overlayPDF, options)
+            .endPage()
+            .endPDF(done);
+    });
 
-        it('Add Overlay from other PDF - scale w/ page_index', (done) => {
-            const src = path.join(__dirname, 'materials/issue-28-input.pdf');
-            const overlayPDF = path.join(__dirname, 'materials/test2.pdf');
-            const output = path.join(__dirname, 'output/Add overlay (#28) with page - scale.pdf');
+    it('Add Overlay from other PDF - stretch to fit  w/ page_index', (done) => {
+        const src = path.join(__dirname, 'materials/issue-28-input.pdf');
+        const overlayPDF = path.join(__dirname, 'materials/test2.pdf');
+        const output = path.join(__dirname, 'output/Add overlay (#28) with page - stretch.pdf');
 
-            const recipe = new HummusRecipe(src, output);
-            const options = {
-                keepAspectRatio: true,
-                scale: 3,
-                page:2
-            };
-            recipe
-                .editPage(1)
-                .overlay(overlayPDF, options)
-                .endPage()
-                .endPDF(done);
-        });
-
-        it('Add Overlay from other PDF - fitWidth w/ page_index', (done) => {
-            const src = path.join(__dirname, 'materials/issue-28-input.pdf');
-            const overlayPDF = path.join(__dirname, 'materials/test2.pdf');
-            const output = path.join(__dirname, 'output/Add overlay (#28) with page - fitWidth.pdf');
-
-            const recipe = new HummusRecipe(src, output);
-            const options = {
-                keepAspectRatio: true,
-                fitWidth: true,
-                page:2
-            };
-            recipe
-                .editPage(1)
-                .overlay(overlayPDF, options)
-                .endPage()
-                .endPDF(done);
-        });
-
-        it('Add Overlay from other PDF - fitHeight w/ page_index', (done) => {
-            const src = path.join(__dirname, 'materials/issue-28-input.pdf');
-            const overlayPDF = path.join(__dirname, 'materials/test2.pdf');
-            const output = path.join(__dirname, 'output/Add overlay (#28) with page - fitHeight.pdf');
-
-            const recipe = new HummusRecipe(src, output);
-            const options = {
-                keepAspectRatio: true,
-                fitHeight: true,
-                page:1
-            };
-            recipe
-                .editPage(1)
-                .overlay(overlayPDF, options)
-                .endPage()
-                .endPDF(done);
-        });
-
-        it('Add Overlay from other PDF - stretch to fit  w/ page_index', (done) => {
-            const src = path.join(__dirname, 'materials/issue-28-input.pdf');
-            const overlayPDF = path.join(__dirname, 'materials/test2.pdf');
-            const output = path.join(__dirname, 'output/Add overlay (#28) with page - stretch.pdf');
-
-            const recipe = new HummusRecipe(src, output);
-            const options = {
-                fitHeight: true,
-                fitWidth: true,
-                page:2
-            };
-            recipe
-                .editPage(1)
-                .overlay(overlayPDF, options)
-                .endPage()
-                .endPDF(done);
-        });
-
-
-
-
-
-
-
-
+        const recipe = new HummusRecipe(src, output);
+        const options = {
+            fitHeight: true,
+            fitWidth: true,
+            page: 2
+        };
+        recipe
+            .editPage(1)
+            .overlay(overlayPDF, options)
+            .endPage()
+            .endPDF(done);
+    });
 });
