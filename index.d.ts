9e739171f683299ae295245c037e8e92b1ba5439
Lee James Doran-Constant <easyTree@gmail.com>
2022-08-19 10:22:12 -0400

    TextOptions.hilite.color

--- index.d.ts
+++ index.d.ts
@@ -139,7 +139,7 @@ declare namespace Recipe {
         layout?: number|string;
         overflow?: () => void
         hilite?: boolean|{
-            color?: string;
+            color?: string|number[];
             opacity?: number;
         };
         textBox?: TextBox;
