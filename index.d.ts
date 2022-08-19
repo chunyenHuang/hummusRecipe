f9d0ad42fa5ede1efad7f567c131c031cc815792
Lee James Doran-Constant <easyTree@gmail.com>
2022-08-19 09:49:53 -0400

    Update paper sizes correctly

--- index.d.ts
+++ index.d.ts
@@ -237,7 +237,7 @@ declare class Recipe {
 
     createPage(pageWidth: number, pageHeight: number): Recipe;
 
-    createPage(pageSize: 'executive' |'folio' |'legal' |'letter' |'ledger' |'tabloid' |'a0-a10' |'b0-b10' |'c0-c10' |'ra0-ra4' |'sra0-ara4' = 'letter'): Recipe;
+    createPage(pageSize: 'executive' |'folio' |'legal' |'letter' |'ledger' |'tabloid' |'a0' | 'a1' | 'a2' | 'a3' | 'a4' | 'a5' | 'a6' | 'a7' | 'a8' | 'a9' | 'a10' |'b0' | 'b1' | 'b2' | 'b3' | 'b4' | 'b5' | 'b6' | 'b7' | 'b8' | 'b9' | 'b10'  |'c0' | 'c1' | 'c2' | 'c3' | 'c4' | 'c5' | 'c6' | 'c7' | 'c8' | 'c9' | 'c10'  |'ra0' | 'ra1' | 'ra2' | 'ra3' | 'ra4' |'sra0-ara4' = 'letter'): Recipe;
 
     endPage(): Recipe;
 
